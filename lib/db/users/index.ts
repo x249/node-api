8cdd30770448be8d9ddeebce61822d37bb3018da
phr3nzy <adilosama47@gmail.com>
2019-05-12 16:11:08 +0200

    Add: mongodb user model

---
+++ lib/db/users/index.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+	username: {
+		type: String,
+		required: [true, "Username is required"],
+		unique: true,
+		trim: true
+	},,
+	firstName: {
+		type: String,
+		required: [true, "First name is required"]
+	},
+	lastName: {
+		type: String,
+		required: [true, "Last name is required"]
+	},
+	email: {
+		type: String,
+		required: [true, "Email is required"],
+		unique: true,
+		trim: true
+	},
+	password: {
+		type: String,
+		required: [true, "Password is required"],
+		unique: true,
+		trim: true
+	},
+	role: {
+		type: String,
+		enum: ["User", "Admin"],
+		required: [true, "User type is required"]
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now
+	},
+	updatedAt: {
+		type: Date
+	}
+});
+
+userSchema.set("toJSON", { virtuals: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
