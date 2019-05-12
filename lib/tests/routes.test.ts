c74c484fbcd41e7bee1b0e289ef86c8ec0bd5416
phr3nzy <adilosama47@gmail.com>
2019-05-12 17:10:30 +0200

    Feat: add Unit and Integration testing

---
+++ lib/tests/routes.test.ts
@@ -0,0 +1,32 @@
+
+import express, { Router } from "express";
+import request from "supertest";
+import { applyMiddleware, applyRoutes } from "../utils";
+import promiseRequest from "request-promise";
+import middleware from "../middleware";
+import errorHandlers from "../middleware/errorHandler";
+import routes from "../routes/main";
+
+jest.mock("request-promise");
+(promiseRequest as any).mockImplementation(() => '{"features": []}');
+
+describe("routes", () => {
+	let router: Router;
+
+	beforeEach(() => {
+		router = express();
+		applyMiddleware(middleware, router);
+		applyRoutes(routes, router);
+		applyMiddleware(errorHandlers, router);
+	});
+
+	test("api health check", async () => {
+		const response = await request(router).get("/");
+		expect(response.status).toEqual(200);
+	});
+
+	test("a non-existing api method", async () => {
+		const response = await request(router).get("/api/v12/non-existing-api-route");
+		expect(response.status).toEqual(401);
+	});
+});
