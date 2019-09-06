e61a51be79d8d7e6831f694e9e4b471b71915579
phr3nzy <adilosama47@gmail.com>
2019-09-06 12:15:14 +0200

    Fix: jest not exiting bug due to mongodb connection

--- lib/tests/routes.test.ts
+++ lib/tests/routes.test.ts
@@ -5,6 +5,7 @@ import { applyMiddleware, applyRoutes } from "../utils";
 import middleware from "../middleware";
 import errorHandlers from "../middleware/errorHandler";
 import routes from "../routes/main";
+import mongoose from 'mongoose';
 
 
 describe("routes", () => {
@@ -17,15 +18,21 @@ describe("routes", () => {
 		applyMiddleware(errorHandlers, router);
 	});
 
-	test("api health check", async () => {
+	test("api health check", async (done) => {
 		const response = await request(router).get("/");
 		expect(response.status).toEqual(200);
 		done();
 	});
 
-	test("a non-existing api method", async () => {
+	test("a non-existing api method", async (done) => {
 		const response = await request(router).get("/api/v12/non-existing-api-route");
 		expect(response.status).toEqual(401);
+		done();
+	});
+
+	afterAll(async done => {
+		// Closing the DB connection allows Jest to exit successfully.
+		await mongoose.connection.close()
 		done();
 	});
 });
