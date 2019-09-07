c343537614e0f5c925bd82bd6055347a355f78b8
phr3nzy <adilosama47@gmail.com>
2019-09-07 16:46:37 +0200

    Add: more user controller test cases

--- lib/tests/controller.test.ts
+++ lib/tests/controller.test.ts
@@ -18,12 +18,35 @@ describe("controller", () => {
 		});
 		done();
 	});
+
+	test("create an existing user", async done => {
+		const user = await newUser({
+			email: "test@testing.com",
+			firstName: "John",
+			lastName: "Doe",
+			password: "testing123",
+			role: "User",
+			username: "jd12345"
+		});
+		expect(user).toStrictEqual({ error: "User already exists", status: 400 });
+		done();
+	});
+
 	test("authenticate existing user", async done => {
 		const auth: any = await authenticateUser({
 			username: "jd12345",
 			password: "testing123"
 		});
 		expect(auth.status).toEqual(200);
+		done();
+	});
+
+	test("authenticate a non existing user", async done => {
+		const auth: any = await authenticateUser({
+			username: "dj12345",
+			password: "testing123"
+		});
+		expect(auth.status).toEqual(404);
 		done();
 	});
 
