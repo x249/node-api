a7221e8cbc701bff885746e7e577a7bd823b6806
phr3nzy <adilosama47@gmail.com>
2019-09-11 18:53:40 +0200

    Refactor: arrange code alphabetically

--- lib/db/users/index.ts
+++ lib/db/users/index.ts
@@ -1,54 +1,54 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface DBUserInterface extends Document {
-    username: string;
+    createdAt: Date;
+    email: string;
     firstName: string;
     lastName: string;
-    email: string;
     password: string;
     role: string;
-    createdAt: Date;
     updatedAt: Date;
+    username: string;
 }
 
 const userSchema: Schema = new Schema({
     createdAt: {
-        type: Date,
         default: Date.now,
+        type: Date,
     },
     email: {
-        type: String,
         required: [true, 'Email is required'],
-        unique: true,
         trim: true,
+        type: String,
+        unique: true,
     },
     firstName: {
-        type: String,
         required: [true, 'First name is required'],
+        type: String,
     },
     lastName: {
         type: String,
         required: [true, 'Last name is required'],
     },
     password: {
-        type: String,
         required: [true, 'Password is required'],
-        unique: true,
         trim: true,
+        type: String,
+        unique: true,
     },
     role: {
-        type: String,
         enum: ['User', 'Admin'],
         required: [true, 'User type is required'],
+        type: String,
     },
     updatedAt: {
         type: Date,
     },
     username: {
-        type: String,
         required: [true, 'Username is required'],
-        unique: true,
         trim: true,
+        type: String,
+        unique: true,
     },
 });
 
