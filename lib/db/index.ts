711961a693b4d8706525b9aea27ae4d687bb088b
phr3nzy <adilosama47@gmail.com>
2019-12-23 01:11:19 +0200

    Edit: optimize mongo connection options for production

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,9 +1,21 @@
 import mongoose from 'mongoose';
 import { DB_URL } from '../config';
 import User, { DBUserInterface } from './users';
+import { NODE_ENV } from '../config';
 
 try {
-    mongoose.connect(DB_URL, { useNewUrlParser: true, useCreateIndex: true });
+    mongoose.connect(DB_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        autoIndex: NODE_ENV === 'production' ? false : true,
+        poolSize: NODE_ENV === 'production' ? 10 : 5,
+        reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+        reconnectInterval: 500, // Reconnect every 500ms
+        connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+        family: 4, // Use IPv4, skip trying IPv6
+    });
 } catch (err) {
     console.log(err);
 }
