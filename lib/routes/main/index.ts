86c168c41c0134b9ccac56950e9887b582cd6ff6
phr3nzy <adilosama47@gmail.com>
2020-02-16 18:17:35 +0200

    Add: func type on main routes handler func's

--- lib/routes/main/index.ts
+++ lib/routes/main/index.ts
@@ -1,18 +1,19 @@
 import { Request, Response } from 'express';
 import { API_VER } from '../../config';
+import { HandlerFunctionType } from '../../types/middleware';
 
 export const mainRoutes = [
     {
-        handler: async (req: Request, res: Response) => {
+        handler: (async (req: Request, res: Response) => {
             res.send('Node Backend API');
-        },
+        }) as HandlerFunctionType,
         method: 'get',
         path: '/',
     },
     {
-        handler: async (req: Request, res: Response) => {
+        handler: (async (req: Request, res: Response) => {
             res.send(`API Version: ${API_VER}`);
-        },
+        }) as HandlerFunctionType,
         method: 'get',
         path: '/api/v1/version',
     },
